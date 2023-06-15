b4cd810d4d7618f7b534a8b17a4e4dc817b6c4d2
Youngju-Jang <joj1043@nate.com>
2023-06-15 09:25:30 +0900

    [Feat] useScroll
    related to #9

---
+++ useScroll.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+import { useEffect, useState, useRef } from "react";
+
+const useScoroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0,
+  });
+  const onScroll = () => {
+    setState({ y: window.scrollY, x: window.scrollX });
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onscroll);
+  }, []);
+  return state;
+};
+
+export default function App() {
+  const { y } = useScoroll();
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+    </div>
+  );
+}
