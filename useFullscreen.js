482549eabae8a339a53c96dc8c9991c954a8162a
Youngju-Jang <joj1043@nate.com>
2023-06-15 09:49:12 +0900

    [Feat] useFullscreen
    related to #9

---
+++ useFullscreen.js
@@ -0,0 +1,56 @@
+import "./styles.css";
+import { useEffect, useState, useRef } from "react";
+
+const useFullscreen = (callback) => {
+  const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullScreen) {
+        element.current.mozRequestFullScreen();
+      } else if (element.current.webkitRequestFullscreen) {
+        element.current.webkitRequestFullscreen();
+      } else if (element.current.msRequestFullscreen) {
+        element.current.msRequestFullscreen();
+      }
+      runCb(true);
+    }
+  };
+  const exitFull = () => {
+    document.exitFullscreen();
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    runCb(false);
+  };
+  return { element, triggerFull, exitFull };
+};
+
+export default function App() {
+  const onFullS = (isFull) => {
+    console.log(isFull ? "we are full" : "we are samll");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <img
+        ref={element}
+        src="https://cdnimage.dailian.co.kr/news/201407/news_1405924302_449061_m_1.jpg"
+      />
+      <button onClick={triggerFull}>Make fullcreen</button>
+      <button onClick={exitFull}>Exit fullcreen</button>
+    </div>
+  );
+}
